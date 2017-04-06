b76fcd818b8fceeeb9d74e30724fd50f436add42
alpha <alfaghost1@gmail.com>
2017-04-06 23:51:20 +0300

    Add categorization

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :authorships
+  has_many :categorizations
+  has_many :categories, :through => :categorizations
   has_many :authors, :through => :authorships
 
   default_scope { order("id DESC") }
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
+  has_many :categorizations
+  has_many :books, :through => :categorizations
+
+
   default_scope { order("name ASC") }
 end
